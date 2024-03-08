b7df77b2c77325ce493ff423dee8f1a072305fd7
Phạm Thái Sơn <swlpham@gmail.com>
2024-03-09 01:57:15 +0700

    wrap application in cron job that runs at midnight everyday

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import "reflect-metadata";
 import { BalanceCalc } from "./business";
 import { connectDB } from "./infra";
 import {BalanceRepo} from "./entity";
+import { CronJob } from "cron";
 
 const start = async () => {
     const db = await connectDB();
     const balanceRepo = new BalanceRepo(db);
     const service = new BalanceCalc(balanceRepo);
     try {
+        console.log("start process daily balance");
         await service.processDailyCalcBalance();
     } catch (e) {
         console.log("failed process withdrawal calculation: ", e);
@@ -17,4 +19,12 @@ const start = async () => {
     await db.destroy();
 };
 
-start();
+new CronJob("0 0 0 * * *", async () => {
+    console.log("starting job");
+    await start();
+},
+() => {
+    console.log("finished job");
+},
+true,
+"UTC+7");
